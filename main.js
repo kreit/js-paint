6177d26ffc718e261f25f163c9eb3363736ce1ae
kreit <reitenbaughkm@gmail.com>
2024-12-02 08:50:04 -0600

    On branch master  Changes to be committed: 	modified:   index.html 	modified:   main.js 	modified:   style.css

--- main.js
+++ main.js
@@ -2,27 +2,15 @@
  * OUR HELPER CODE *
 *******************/
 
-/*
- * Here we add the squares to the canvas dynamically.
- * You can mostly leave this section alone!
- * But if you want to change how wide the canvas is,
- * there are just two steps:
- * 
- * 1. Change the `gridWidth` value below.
- * 2. Change the `grid-template-rows` and
- * `grid-template-columns` to match.
- *
- * To make the second one happen, the number to change
- * is the first argument to `repeat`, currently set at 10.
- */
 const gridWidth = 10;
 let count = 0;
 while (count <= gridWidth * gridWidth) {
-  const canvas = document.querySelector('.canvas');
-  const div = document.createElement('div');
-  div.className = 'square color-5';
-  canvas.appendChild(div);
-  count++;
+const canvas = document.querySelector('.canvas');
+let isMouseDown = false;
+const square = document.createElement('div');
+square.classList.add('square');
+canvas.appendChild(square);
+count++;
 }
 
 // You probably should NOT do these in the order below.
@@ -39,35 +27,52 @@ while (count <= gridWidth * gridWidth) {
 // And proceed from there to getting the squares working.
 //
 
-// ALSO.
-// You do not have to follow the sections below. If you're doing your functions inline, it doesn't make a lot of sense to separate the event listener functions from their wiring!
-
-/***********
- * QUERIES *
-***********/
-
-// Add queries for all your squares, palette colors, and brush here.
-// (Note the singular or plural used in that sentence!)
-
-
+const squares = document.querySelectorAll('.square');
+const paletteColors = document.querySelectorAll('.palette-color');
+const brush = document.querySelector('#current-brush');
 
 /****************************
  * EVENT LISTENER FUNCTIONS *
 ****************************/
 
-// Now add some functions to handle clicking one particular square
-// and clicking one particular palette color. You can leave them
-// empty at first, though a console.log just to know they're being
-// run as event listeners (after the next step is set up) isn't a
-// bad idea for testing purposes.
+function handleSquareClick(event) {
+  const currentColor = brush.classList[1];
+  event.target.classList.replace(event.target.classList[1], currentColor);
+}
 
+function handlePaletteClick(event) {
+  const newColor = event.target.classList[1];
+  brush.classList.replace(brush.classList[1], newColor);
+}
 
+function handleMouseEnter(event) {
+  if (isMouseDown) {
+    const currentColor = brush.classList[1];
+    event.target.classList.replace(event.target.classList[1], currentColor);
+  }
+}
+
+function handleMouseDown() {
+  isMouseDown = true;
+}
+
+function handleMouseUp() {
+  isMouseDown = false;
+}
 
 /**************************
  * WIRING IT ALL TOGETHER *
 **************************/
 
-// Now: wiring up our event listeners to our html node elements.
-// You'll need to add the appropriate event listener for each
-// square and for each palette color from the functions you
-// wrote above.
+
+squares.forEach(square => {
+  square.addEventListener('click', handleSquareClick);
+  square.addEventListener('mouseenter', handleMouseEnter);
+});
+
+paletteColors.forEach(color => {
+  color.addEventListener('click', handlePaletteClick);
+});
+
+document.addEventListener('mousedown', handleMouseDown);
+document.addEventListener('mouseup', handleMouseUp);
